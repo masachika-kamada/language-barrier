53d096a26773a8272a04f105a1bd6dc11eca5ee6
masachika <masachika.kmd@gmail.com>
2022-08-08 00:07:21 +0900

    ocrのリファクタリング

--- image_ocr.py
+++ image_ocr.py
@@ -4,20 +4,12 @@ import pytesseract
 
 
 def run():
-    st.title("OCR Programa")
-    st.write("Optical Character Recognition (OCR) implementado com Python")
-    imagem = st.file_uploader("Selecione alguma imagem", type=["png","jpg"])
-    #se selecionar alguma imagem...
-    if imagem:
-        img = Image.open(imagem)
+    st.title("Image OCR")
+    image = st.file_uploader("select image", type=["png","jpg"])
+    if image:
+        img = Image.open(image)
         st.image(img, width=350)
-        st.info("Texto extraído")
-        texto = extrair_texto(img)
-        st.write("{}".format(texto))
-
-        analisar_texto = st.sidebar.checkbox("Analisar texto")
-
-
-def extrair_texto(img):
-    texto = pytesseract.image_to_string(img, lang="eng")
-    return texto
+        txt = pytesseract.image_to_string(img, lang="eng")
+        print(txt)
+        print(len(txt))
+        st.write(" ".join(txt))
